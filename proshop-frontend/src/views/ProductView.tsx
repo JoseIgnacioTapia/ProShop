a10a931c03f27fb6324fc43dab0ab8f7450384e4
JoseIgnacioTapia <josignaciot@gmail.com>
2023-12-01 16:07:42 -0300

    Fetch single product

--- proshop-frontend/src/views/ProductView.tsx
+++ proshop-frontend/src/views/ProductView.tsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import axios from "axios";
 import Rating from "../components/Rating";
-import products from "../productsTypes";
+import { RootObject } from "../productsTypes";
 
 const ProductView = () => {
+  const [product, setProduct] = useState<RootObject>({});
   const { id: productId } = useParams();
-  const product = products.find((product) => product._id === productId);
-  console.log(product);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(
+        `http://localhost:8000/api/products/${productId}`
+      );
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
